d51b08671ea49e036b3cfd5bd5fbb6c2f9a9ab48
adria <adria@DOOMGUY-PC>
2022-04-08 02:38:13 -0300

    Adicionar arquivos de projeto.

---
+++ ArrayInstancias/Estudante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayInstancias
+{
+    class Estudante
+    {
+        public String Nome { get; set; }
+
+        public String Email { get; set; }
+
+        public override string ToString()
+        {
+            return " Nome do estudante: "
+                + Nome
+                +" email do estudante: "
+                + Email;
+        }
+    }
+}
---
+++ ArrayInstancias/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArrayInstancias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quarto;
+            Estudante[] quartosDisponiveis = new Estudante[10];
+
+            Console.WriteLine("Digite a quantidade de quartos a serem alugados");
+            quarto = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quarto; i++)
+            {
+                Console.WriteLine("Digite o nome do estudante:");
+                String nome = Console.ReadLine();
+                Console.WriteLine("Digite o email do estudante:");
+                String email = Console.ReadLine();
+                Console.WriteLine("Digite o numero do quarto que o estudante deseja alugar:");
+                int nQuarto = int.Parse(Console.ReadLine());
+                quartosDisponiveis[nQuarto] = new Estudante{Nome = nome, Email = email};
+            }
+
+            Console.WriteLine("Os quartos alugados sâo:");
+            for (int i = 0; i < quartosDisponiveis.Length; i++)
+            {
+                if (quartosDisponiveis[i] != null) 
+                {
+                    Console.WriteLine("Quarto: "+i
+                        + quartosDisponiveis[i].ToString());
+
+                }
+            }  
+        }
+    }
+}
